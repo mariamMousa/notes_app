8c07926127f1e3b4d01dd1b87d726af9d0489725
mariamMousa <mariam.mousa.almohammad@gmail.com>
2023-11-22 12:10:38 +0300

    create note model & change font

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Caveat'),
       home: const NotesView(),
     );
   }
