f72daed5ca7032358997c0ec0d2563946521d432
mariamMousa <mariam.mousa.almohammad@gmail.com>
2023-12-04 11:02:06 +0300

    add noteColor to constantss.dart

--- lib/views/constants.dart
+++ lib/views/constants.dart
@@ -2,3 +2,22 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff62FCD7);
 const kNotesBox = 'notes_box';
+const noteColor = [
+  Color(0XFF125631),
+  Color(0XFF000555),
+  Color(0XFF1CCC31),
+  Color(0XFF009991),
+  Color(0XFFD00000),
+  Color(0XFFAAAA00),
+  Color(0XFF1111FF),
+  Color(0XFFEEE000),
+  Color(0XFF00FFDD),
+  Color(0XFFEE0055),
+  Color(0XFFCCD789),
+  Color(0XFFCCCD0C),
+  Color(0XFF00DD00),
+  Color(0XFFCC5555),
+  Color(0XFF1ED0CC),
+  Color(0XFFCC00CC),
+  Color(0XFFFC00CC),
+];
